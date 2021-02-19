d2e7c8535532e1e25cd9f7b279bbf2593e6532e3
Xiaoxuan Lu <xiaoxuan.lu@student.auc.nl>
2021-02-19 15:33:12 +0100

    test

--- hello.s
+++ hello.s
@@ -2,14 +2,14 @@
 
 .global _main # subroutine main 
 
-_main:
+main:
     movq %rsp, %rbp
     mystring: .asciz "Daphne_Xiaoxuan_2644582_2661045_Assignment_1\n" # string with names, student number and assignment
 
     movq $0, %rax # no vector registers in use for printf
     movq $mystring, %rdi  # load address of a string
-    call _printf # Call the printf routine
+    call printf # Call the printf routine
 
 end:
     movq $0, %rdi
-    call _exit
+    call exit
